fdf0e78ee45193d009bd6377550f0888148fb080
MohamedSameeh <taktshfw@gmail.com>
2024-10-13 19:41:35 +0300

    commit

---
+++ src/main/java/com/mycompany/calc/java/CalcJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calc.java;
+
+import jfram.calc;
+
+/**
+ *
+ * @author SAFEER
+ */
+public class CalcJava {
+
+    public static void main(String[] args) {
+        new calc().setVisible(true);
+    }
+}
